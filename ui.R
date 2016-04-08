7193ef94c8d04e1b15f3b6201a40186dcbc00ca0
edgeandpearl <tanglorfirefoot@gmail.com>
2016-04-08 22:53:42 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ L <- read.csv("all_words.csv", sep = ",")
 # Define the overall UI
 shinyUI(
   fluidPage(
-    titlePanel("One thousand Most Frequently Used Words Lalka Edition"),
+    titlePanel("One thousand Most Frequently Used Words HW Edition"),
     
     # Create a new Row in the UI for selectInputs
     fluidRow(
